e9d47d09f3da8ef93dc448fdd0b410e6ef995071
Micah Tinife <micahking2k@gmail.com>
2023-09-13 23:44:45 -0400

    working on routes

--- vue-portfolio/src/App.vue
+++ vue-portfolio/src/App.vue
@@ -3,8 +3,67 @@
 </script>
 
 <template>
-  <h1>New Portfolio Project using vue</h1>
+  <h1>Heading 1 Heading 1 Heading 1 Heading 1 </h1>
+  <h2>Heading 2 Heading 2 Heading 2 Heading 2 </h2>
+  <h3>Heading 3 Heading 3 Heading 3 Heading 3 </h3>
+  <h4>Heading 4 Heading 4 Heading 4 Heading 4 </h4>
+  <p class="font-sz-body-1">I love to learn and try different/new things. My bread and butter is web developer but I work towards being able to being able to be useful in any domain.</p>
+  <p class="font-sz-body-2">I love to learn and try different/new things. My bread and butter is web developer but I work towards being able to being able to be useful in any domain.</p>
+  <p class="font-sz-body-3">I love to learn and try different/new things. My bread and butter is web developer but I work towards being able to being able to be useful in any domain.</p>
+  <p class="font-sz-body-4">I love to learn and try different/new things. My bread and butter is web developer but I work towards being able to being able to be useful in any domain.</p>
+  <p class="font-sz-body-5">I love to learn and try different/new things. My bread and butter is web developer but I work towards being able to being able to be useful in any domain.</p>
+  <p class="font-sz-body-6">I love to learn and try different/new things. My bread and butter is web developer but I work towards being able to being able to be useful in any domain.</p>
 </template>
 
 <style scoped>
+  h1 {
+    font-family: var(--font-bebas);
+    font-size: var(--font-sz-heading-1);
+    line-height: var(--line-height);
+  }
+  h2 {
+    font-family: var(--font-bebas);
+    font-size: var(--font-sz-heading-2);
+    line-height: var(--line-height);
+   }
+  h3 {
+    font-family: var(--font-bebas);
+    font-size: var(--font-sz-heading-3);
+    line-height: var(--line-height);
+  }
+  h4 {
+    font-family: var(--font-bebas);
+    font-size: var(--font-sz-heading-4);
+    line-height: var(--line-height);
+  }
+  .font-sz-body-1 {
+    font-family: var(--font-roboto);
+    font-size: var(--font-sz-body-1);
+    line-height: var(--line-height);
+  }
+  .font-sz-body-2 {
+    font-family: var(--font-roboto);
+    font-size: var(--font-sz-body-2);
+    line-height: var(--line-height);
+  }
+  .font-sz-body-3 {
+    font-family: var(--font-roboto);
+    font-size: var(--font-sz-body-3);
+    line-height: var(--line-height);
+  }
+  .font-sz-body-4 {
+    font-family: var(--font-roboto);
+    font-size: var(--font-sz-body-4);
+    line-height: var(--line-height);
+  }
+  .font-sz-body-5 {
+    font-family: var(--font-roboto);
+    font-size: var(--font-sz-body-5);
+    line-height: var(--line-height);
+  }
+  .font-sz-body-6 {
+    font-family: var(--font-roboto);
+    font-size: var(--font-sz-body-6);
+    line-height: var(--line-height);
+  }
 </style>
