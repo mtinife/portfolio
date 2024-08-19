fb9ca14facbae5d4bb34bd604d6661cac20a6f7f
Micah M Tinife <micahking2k@gmail.com>
2024-08-18 23:41:05 -0400

    Updated Styles and added files

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,3 +1,63 @@
+function backgroundContainer() {
+	const backgroundGroup = document.querySelector(
+		'.background-group'
+	);
+	for (let i = 1; i < 20; i++) {
+		const containerId = `iconContainer${i}`;
+		const container = document.createElement('div');
+		container.className = 'iconContainer';
+		container.id = containerId;
+		backgroundGroup.appendChild(container);
+		addIcons(containerId);
+	}
+
+	function addIcons(containerId) {
+		const iconContainer =
+			document.getElementById(containerId);
+		const iconClass = [
+			'devicon-amazonwebservices-plain-wordmark',
+			'devicon-angular-plain',
+			'devicon-archlinux-plain',
+			'devicon-bamboo-original',
+			'devicon-c-plain',
+			'devicon-cplusplus-plain',
+			'devicon-css3-plain',
+			'devicon-docker-plain',
+			'devicon-django-plain',
+			'devicon-firebase-plain',
+			'devicon-git-plain',
+			'devicon-html5-plain',
+			'devicon-javascript-plain',
+			'devicon-jest-plain',
+			'devicon-jquery-plain',
+			'devicon-kotlin-plain',
+			'devicon-linux-plain',
+			'devicon-nodejs-plain',
+			'devicon-php-plain',
+			'devicon-postgresql-plain',
+			'devicon-python-plain',
+			'devicon-react-original',
+			'devicon-tailwindcss-original',
+			'devicon-typescript-plain',
+			'devicon-vim-plain',
+			'devicon-vuejs-plain',
+		];
+
+		for (let i = 0; i < 50; i++) {
+			const icon = document.createElement('i');
+			icon.className = `${randomIcon(iconClass)}`;
+			iconContainer.appendChild(icon);
+		}
+	}
+
+	function randomIcon(values) {
+		const randomIndex = Math.floor(
+			Math.random() * values.length
+		);
+		return values[randomIndex];
+	}
+}
+
 function tagCloud() {
 	var texts = [
 		'JavaScript',
@@ -5,16 +65,18 @@ function tagCloud() {
 		'HTML',
 		'C',
 		'C++',
-		'React',
+		'VueJS',
+		'React JS',
 		'Python',
 		'Java',
 		'git',
-		'django',
+		'Vim',
+		'Django',
 		'Node.js',
-		'OpenCV',
-		'GCP',
 		'MySQL',
 		'jQuery',
+		'AWS',
+		'PHP',
 	];
 	var tc = TagCloud('.content', texts);
 	console.log(tc);
@@ -33,6 +95,7 @@ function footerElm() {
 function init() {
 	tagCloud();
 	footerElm();
+	backgroundContainer();
 }
 
 init();
