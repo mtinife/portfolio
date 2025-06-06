e0c473def5e90a09f852d600c031fb02641f214b
Micah Tinife <micahking2k@gmail.com>
2025-06-05 20:00:26 -0400

    portfolio update 05/2025

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,63 +1,3 @@
-function backgroundContainer() {
-	const backgroundGroup = document.querySelector(
-		'.background-group'
-	);
-	for (let i = 1; i < 20; i++) {
-		const containerId = `iconContainer${i}`;
-		const container = document.createElement('div');
-		container.className = 'iconContainer';
-		container.id = containerId;
-		backgroundGroup.appendChild(container);
-		addIcons(containerId);
-	}
-
-	function addIcons(containerId) {
-		const iconContainer =
-			document.getElementById(containerId);
-		const iconClass = [
-			'devicon-amazonwebservices-plain-wordmark',
-			'devicon-angular-plain',
-			'devicon-archlinux-plain',
-			'devicon-bamboo-original',
-			'devicon-c-plain',
-			'devicon-cplusplus-plain',
-			'devicon-css3-plain',
-			'devicon-docker-plain',
-			'devicon-django-plain',
-			'devicon-firebase-plain',
-			'devicon-git-plain',
-			'devicon-html5-plain',
-			'devicon-javascript-plain',
-			'devicon-jest-plain',
-			'devicon-jquery-plain',
-			'devicon-kotlin-plain',
-			'devicon-linux-plain',
-			'devicon-nodejs-plain',
-			'devicon-php-plain',
-			'devicon-postgresql-plain',
-			'devicon-python-plain',
-			'devicon-react-original',
-			'devicon-tailwindcss-original',
-			'devicon-typescript-plain',
-			'devicon-vim-plain',
-			'devicon-vuejs-plain',
-		];
-
-		for (let i = 0; i < 50; i++) {
-			const icon = document.createElement('i');
-			icon.className = `${randomIcon(iconClass)}`;
-			iconContainer.appendChild(icon);
-		}
-	}
-
-	function randomIcon(values) {
-		const randomIndex = Math.floor(
-			Math.random() * values.length
-		);
-		return values[randomIndex];
-	}
-}
-
 function tagCloud() {
 	var texts = [
 		'JavaScript',
@@ -82,20 +22,97 @@ function tagCloud() {
 	console.log(tc);
 }
 
-function footerElm() {
-	let footer = document.querySelector('footer');
+function copyrightElm() {
+	let copyright = document.querySelector('.main__text__copyright');
 	let paraElm = document.createElement('p');
 	let date = new Date();
 	let getCurrentYear = date.getFullYear();
-	paraElm.innerHTML = `\u00A9 ${getCurrentYear} - Micah M Tinife`;
-	paraElm.classList = 'container';
-	footer.appendChild(paraElm);
+	paraElm.innerHTML = `\u00A9 ${getCurrentYear} - All Rights Reserved.`;
+	copyright.appendChild(paraElm);
+}
+
+function addExpYears() {
+  const now = new Date();
+  const firstJobYear = new Date('2019-10-28');
+
+  const diffInMs = now.getTime() - firstJobYear.getTime();
+  const diffInYears = diffInMs / (1000 * 60 * 60 * 24 * 365.25);
+
+  const expYear = Math.floor(diffInYears);
+  return expYear;
+}
+
+function expYearsDisplay() {
+  const expYearShowcase = document.querySelector('#expYearShowcase');
+  expYearShowcase.innerHTML = `${addExpYears()}+`;
+}
+
+function typeWriter() {
+  const roles = document.getElementById('typewriter__role');
+  const exp = document.getElementById('typewriter__exp');
+  const lang = document.getElementById('typewriter__lang');
+
+  const typewriter1 = new Typewriter (roles, {
+    loop: true,
+    delay: 75,
+  })
+  typewriter1
+    .pauseFor(2500)
+    .typeString('software engineer')
+    .pauseFor(300)
+    .deleteChars(17)
+    .typeString('designer')
+    .pauseFor(300)
+    .deleteChars(8)
+    .typeString('front-end developer')
+    .pauseFor(300)
+    .deleteChars(19)
+    .typeString('back-end developer')
+    .pauseFor(300)
+    .deleteChars(18)
+    .typeString('web designer')
+    .pauseFor(300)
+    .deleteChars(12)
+    .typeString('web developer')
+    .pauseFor(300)
+    .start()
+}
+
+function infinitScroll() {
+  const scrollers = document.querySelectorAll(".scroller");
+
+  // If a user hasn't opted in for recuded motion, then we add the animation
+  if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+  }
+
+  function addAnimation() {
+    scrollers.forEach((scroller) => {
+      // add data-animated="true" to every `.scroller` on the page
+      scroller.setAttribute("data-animated", true);
+
+      // Make an array from the elements within `.scroller-inner`
+      const scrollerInner = scroller.querySelector(".scroller__inner");
+      const scrollerContent = Array.from(scrollerInner.children);
+
+      // For each item in the array, clone it
+      // add aria-hidden to it
+      // add it into the `.scroller-inner`
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        duplicatedItem.setAttribute("aria-hidden", true);
+        scrollerInner.appendChild(duplicatedItem);
+      });
+    });
+  }
 }
 
 function init() {
 	tagCloud();
-	footerElm();
-	backgroundContainer();
+  typeWriter();
+  infinitScroll();
+  expYearsDisplay();
+  copyrightElm();
 }
 
 init();
